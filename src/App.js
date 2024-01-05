631ae35ed14239583431cc6281bee9bc77748ea1
rrauf8178 <rrauf8178@gmail.com>
2024-01-06 00:24:50 +0400

    add context and create search form

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import "./App.css";
+import { AppProvider } from "./context/context";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+    <AppProvider>
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
         </Routes>
       </BrowserRouter>
+    </AppProvider>
+      
     </>
   );
 }
